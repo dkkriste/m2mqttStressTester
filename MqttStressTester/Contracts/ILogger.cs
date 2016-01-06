ca1c7e21f74f4ef982a5c1293112a235ae0e7512
David Kai Christen Kristensen <davidk@mesan.no>
2016-01-06 16:40:04 +0100

    missing file

---
+++ MqttStressTester/Contracts/ILogger.cs
@@ -0,0 +1,13 @@
+﻿namespace MqttStressTester.Contracts
+{
+    using System;
+
+    public interface ILogger
+    {
+        void LogException(Exception exception);
+
+        void LogEvent(string eventName, string message);
+
+        void LogMetric(string metricName, double value);
+    }
+}
